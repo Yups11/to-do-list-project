fe5a1d21e5d2785758a47004fc956f33d45b5fe6
Elnur Nacu <yupsdigital@yahoo.com>
2024-06-11 11:42:35 +0000

    Add javascript code to create list of tasks from input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const todoValue = document.getElementById("todoText"),
-listItems = document.getElementById("list-items"),
-addUpdateClick = document.getElementById("AddUpdateClick");
+    listItems = document.getElementById("list-items"),
+    addUpdateClick = document.getElementById("AddUpdateClick");
 
 function CreateToDoData() {
     if (todoValue.value === "") {
-        alert("Please enter your task!");
+        alert("Your task is empty!");
         todoValue.focus();
     }
+
+    let li = document.createElement("li");
+    const todoItems = `<div>${todoValue.value}</div>`;
+
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+    todoValue.value = "";
 }
