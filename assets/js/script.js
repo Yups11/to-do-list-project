968cdf17fca5e9340bcbc2c5063ffa14d8dad008
Elnur Nacu <yupsdigital@yahoo.com>
2024-06-11 11:29:45 +0000

    Code in javascript to set alert if input task is empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello to my project"); 
+const todoValue = document.getElementById("todoText"),
+listItems = document.getElementById("list-items"),
+addUpdateClick = document.getElementById("AddUpdateClick");
+
+function CreateToDoData() {
+    if (todoValue.value === "") {
+        alert("Please enter your task!");
+        todoValue.focus();
+    }
+}
